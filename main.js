3cda5f08d4780ebf64d49f40acf737afeeb0bd71
PierreBarge <pierrebarge@hotmail.fr>
2024-04-20 18:49:54 +0200

    It works!!

--- main.js
+++ main.js
@@ -1,44 +1,32 @@
-const checker = () => {
-  setTimeout(() => {
-    let check = 0;
-    for (let i = 0; i < document.querySelectorAll(".progress").length; i++) {
-      if (document.querySelectorAll(".progress")[i].value >= 100) {
-        check = check + 1;
-      }
-    }
-    if (check === 7) {
-      document.querySelector("#begin").textContent =
-        "Every loading is complete";
-    } else {
-      checker();
-    }
-  }, 100);
-};
-
-const timerGoing = () => {
-  let delay = Math.floor(Math.random() * 2000);
-  setTimeout(() => {
-    for (let i = 0; i < document.querySelectorAll(".progress").length; i++) {
-      if (document.querySelectorAll(".progress")[i].value < 100) {
-        document.querySelector(`#status-${i + 1}`).textContent =
-          "In progress...";
-        document.querySelectorAll(".progress")[i].value +=
-          Math.floor(Math.random() * 5) + 1;
-      } else {
-        document.querySelector(`#status-${i + 1}`).textContent = "Complete!";
-      }
-    }
-    timerGoing();
-  }, delay);
-};
+const promises = [];
 
 function startButton() {
-  console.log("Bonjour");
   document.querySelector("#begin").removeEventListener("click", startButton);
   document.querySelector("#begin").textContent = "Loading in progress";
   document.querySelector("#begin").disabled = true;
-  timerGoing();
-  checker();
+  for (let i = 0; i < document.querySelectorAll(".progress").length; i++) {
+    const promise = new Promise((resolve) => {
+      document.querySelector(`#status-${i + 1}`).textContent = "In progress...";
+      function timer() {
+        setTimeout(() => {
+          if (document.querySelectorAll(".progress")[i].value < 100) {
+            document.querySelectorAll(".progress")[i].value +=
+              Math.floor(Math.random() * 5) + 1;
+            timer();
+          } else {
+            document.querySelector(`#status-${i + 1}`).textContent =
+              "Complete!";
+            resolve();
+          }
+        }, Math.floor(Math.random() * 200));
+      }
+      timer();
+    });
+    promises.push(promise);
+  }
+  Promise.all(promises).then(() => {
+    document.querySelector("#begin").textContent = "Every loading is complete";
+  });
 }
 
 document.querySelector("#begin").addEventListener("click", startButton);
